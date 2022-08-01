6e0e536c4698aa412c93e3aec558b04f77e32fb5
pierre <pierre>
2022-08-01 12:57:08 +0200

    get control ranges from dsp

--- src/leap.rs
+++ src/leap.rs
@@ -1,55 +1,12 @@
 use std::{
-    ops::RangeInclusive,
     sync::{mpsc::Receiver, Arc, Mutex},
     thread,
 };
 
 use faust_state::StateHandle;
 use leaprs::*;
-use music_note::midi::MidiNote;
 
-use crate::{dsp, settings::Settings};
-
-fn convert_range(
-    value: f32,
-    input_range: RangeInclusive<f32>,
-    output_range: RangeInclusive<f32>,
-) -> f32 {
-    {
-        let in_min = *input_range.start();
-        let in_max = *input_range.end();
-        let out_min = *output_range.start();
-        let out_max = *output_range.end();
-        ((((value - in_min) * (out_max - out_min)) / (in_max - in_min)) + out_min)
-            .clamp(out_min, out_max)
-    }
-}
-
-/// Smooth step function loosely "sticking" the value to 0 or 1
-/// Assumes that value is between 0 and 1
-/// https://en.wikipedia.org/wiki/Smoothstep
-fn smoothstep(interval: &RangeInclusive<f32>, x: f32) -> f32 {
-    let x = (x - interval.start()) / (interval.end() - interval.start());
-    x * x * (3.0 - 2.0 * x)
-}
-
-pub fn smoothstairs(value: f32, amount: usize, scale: Vec<MidiNote>) -> f32 {
-    let scale: Vec<_> = scale
-        .windows(2)
-        .map(|w| (w[0].into_byte() as f32)..=(w[1].into_byte() as f32))
-        .collect();
-
-    if let Some(interval) = scale.iter().find(|interval| interval.contains(&value)) {
-        let mut value = value;
-
-        for _ in 0..amount {
-            let smooth = smoothstep(interval, value);
-            value = interval.start() + smooth * (interval.end() - interval.start());
-        }
-        return value;
-    }
-    value
-}
+use crate::{dsp::Controls, settings::Settings};
 
 /// Start the leap motion thread
 pub fn start_leap_worker(
@@ -60,7 +17,7 @@ pub fn start_leap_worker(
         let mut connection =
             Connection::create(ConnectionConfig::default()).expect("Failed to connect");
         connection.open().expect("Failed to open the connection");
-        let mut controls = crate::dsp::Controls::default();
+        let mut controls: Controls = { dsp.lock().as_deref().unwrap().into() };
         let mut settings = Settings::default();
         loop {
             if let Ok(message) = connection.poll(1000) {
@@ -79,53 +36,21 @@ pub fn start_leap_worker(
                             HandType::Left => {
                                 let position = hand.palm().position();
 
-                                controls.detune = convert_range(
-                                    position.x(),
-                                    -200.0..=0.0,
-                                    dsp::Controls::detune_range(),
-                                );
-
-                                let raw_note = convert_range(
-                                    position.y(),
-                                    100.0..=600.0,
-                                    dsp::Controls::note_range(),
-                                );
-                                controls.note = smoothstairs(
-                                    raw_note,
-                                    settings.autotune_strength,
-                                    settings.scale_notes(),
-                                );
-
-                                controls.supersaw = convert_range(
-                                    position.z(),
-                                    100.0..=-100.0,
-                                    dsp::Controls::supersaw_range(),
-                                );
+                                controls.detune.set_scaled(position.x(), -200.0..=0.0);
+                                controls
+                                    .note
+                                    .set_scaled(position.y(), 100.0..=600.0, &settings);
+                                controls.supersaw.set_scaled(position.z(), 100.0..=-100.0);
                             }
                             HandType::Right => {
                                 let position = hand.palm().position();
 
-                                controls.cutoff_note = convert_range(
-                                    position.x(),
-                                    0.0..=200.0,
-                                    dsp::Controls::cutoff_range(),
-                                );
-
-                                controls.volume = convert_range(
-                                    position.y(),
-                                    200.0..=300.0,
-                                    dsp::Controls::volume_range(),
-                                );
-                                controls.resonance = convert_range(
-                                    position.z(),
-                                    100.0..=-100.0,
-                                    dsp::Controls::resonance_range(),
-                                );
-                                controls.sub_volume = convert_range(
-                                    hand.grab_angle(),
-                                    0.0..=std::f32::consts::PI,
-                                    dsp::Controls::sub_volume_range(),
-                                );
+                                controls.cutoff_note.set_scaled(position.x(), 0.0..=200.0);
+                                controls.volume.set_scaled(position.y(), 200.0..=300.0);
+                                controls.resonance.set_scaled(position.z(), 100.0..=-100.0);
+                                controls
+                                    .sub_volume
+                                    .set_scaled(hand.grab_angle(), 0.0..=std::f32::consts::PI);
                             }
                         }
                     }
--- src/settings.rs
+++ src/settings.rs
@@ -1,4 +1,9 @@
-use music_note::{midi::MidiNote, Pitch, Scale};
+use std::ops::RangeInclusive;
+
+use music_note::{
+    midi::{MidiNote, Octave},
+    Interval, Pitch, Scale,
+};
 
 /// Application settings
 #[derive(Clone)]
@@ -7,27 +12,41 @@ pub struct Settings {
     /// 0 means no autotune.
     pub autotune_strength: usize,
 
-    /// Root note of the autotune
-    pub root_note: Pitch,
+    /// Root note of the keyboard
+    pub root_note: MidiNote,
+
+    /// Number of octave to display
+    pub octave_range: u8,
 
     /// Scale of the autotune
     pub scale: ScaleType,
 }
 
 impl Settings {
+    pub fn note_range(&self) -> RangeInclusive<u8> {
+        self.root_note.into_byte()
+            ..=(self.root_note + Interval::new(self.octave_range * 12)).into_byte()
+    }
+
+    pub fn note_range_f(&self) -> RangeInclusive<f32> {
+        let range = self.note_range();
+        (*range.start() as f32)..=(*range.end() as f32)
+    }
+
     pub fn octave_notes(&self) -> Vec<Pitch> {
         match self.scale {
             ScaleType::Chromatic => (0..=11).map(Pitch::from_byte).collect(),
-            ScaleType::Major => Scale::major(self.root_note).collect(),
-            ScaleType::Minor => Scale::harmonic_minor(self.root_note).collect(),
-            ScaleType::Blues => Scale::blues(self.root_note).collect(),
+            ScaleType::Major => Scale::major(self.root_note.pitch()).collect(),
+            ScaleType::Minor => Scale::harmonic_minor(self.root_note.pitch()).collect(),
+            ScaleType::Blues => Scale::blues(self.root_note.pitch()).collect(),
         }
     }
 
     /// List all the existing notes of the current
     pub fn scale_notes(&self) -> Vec<MidiNote> {
         let scale = self.octave_notes();
-        (00..=127)
+
+        self.note_range()
             .map(MidiNote::from_byte)
             .filter(|n| scale.contains(&n.pitch()))
             .collect()
@@ -38,7 +57,8 @@ impl Default for Settings {
     fn default() -> Self {
         Self {
             autotune_strength: 2,
-            root_note: Pitch::C,
+            root_note: MidiNote::new(Pitch::C, Octave::TWO),
+            octave_range: 3,
             scale: ScaleType::Chromatic,
         }
     }
