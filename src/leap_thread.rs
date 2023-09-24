47698608870ac7a87e4da348abe9eb682266b819
plule <630159+plule@users.noreply.github.com>
2023-09-24 15:53:16 +0200

    add state to the leap thread

--- src/leap_thread.rs
+++ src/leap_thread.rs
@@ -24,12 +24,12 @@ pub fn run(
             Connection::create(ConnectionConfig::default()).expect("Failed to connect");
         connection.open().expect("Failed to open the connection");
         let mut settings = Settings::default();
+        let mut reader = LeapReader::new(&controls, &ui_tx, &dsp_tx);
         loop {
             if let Some(new_settings) = settings_rx.try_iter().last() {
                 settings = new_settings;
             }
-            if read_leap_and_update(&mut connection, &settings, &controls, &ui_tx, &dsp_tx).is_err()
-            {
+            if reader.read_and_update(&settings, &mut connection).is_err() {
                 // For the lack of better error handling, just assume
                 // that the gui thread quit
                 return;
@@ -38,182 +38,208 @@ pub fn run(
     })
 }
 
-fn read_leap_and_update(
-    connection: &mut Connection,
-    settings: &Settings,
-    controls: &controls::Controls,
-    ui_tx: &Sender<UiUpdate>,
-    dsp_tx: &Sender<dsp_thread::ParameterUpdate>,
-) -> Result<()> {
-    match connection.poll(100) {
-        Ok(message) => {
-            if let Event::Tracking(e) = message.event() {
-                on_tracking_event(e, settings, controls, ui_tx, dsp_tx)?;
-            }
-            ui_tx.send(UiUpdate::ErrorReset)?;
-        }
-        Err(err) => {
-            ui_tx.send(UiUpdate::Error(err.to_string()))?;
-        }
-    }
-    Ok(())
+struct LeapReader<'a> {
+    controls: &'a controls::Controls,
+    ui_tx: &'a Sender<UiUpdate>,
+    dsp_tx: &'a Sender<dsp_thread::ParameterUpdate>,
 }
 
-fn on_tracking_event(
-    e: TrackingEvent<'_>,
-    settings: &Settings,
-    controls: &controls::Controls,
-    ui_tx: &Sender<UiUpdate>,
-    dsp_tx: &Sender<dsp_thread::ParameterUpdate>,
-) -> Result<()> {
-    let preset = &settings.current_preset;
-    let handedness = &settings.system.handedness;
-
-    // Retrieve the current scale 2 by 2 windows
-    let full_scale_window = preset.full_scale_floating_window();
-    let restricted_scale_window = preset.restricted_scale_floating_window();
-
-    // List of visible hands
-    let hands = e.hands();
-    let pitch_hand = hands
-        .iter()
-        .find(|h| h.hand_type() == pitch_hand_type(handedness));
-    let volume_hand = hands
-        .iter()
-        .find(|h| h.hand_type() == volume_hand_type(handedness));
-
-    // Guitar gates
-    let mut guitar_gates = [false, false, false, false];
-    if let Some(hand) = pitch_hand {
-        let position = hand.position_from_body();
-        let velocity = hand.velocity_from_body();
-
-        let note_range = preset.note_range_f();
-
-        // Position the virtual antenna and the pitch hand relative to it
-        let antenna_coord = Vector2::new(400.0, -200.0);
-        let pitch_coord_mm = antenna_coord - Vector2::new(position.x, position.z);
-        // Hand position in semitones
-        let pitch_coord_semitones = pitch_coord_mm / 15.0;
-        let pitch_coord_semitones = Vector2::new(-pitch_coord_semitones.x, pitch_coord_semitones.y);
-        // Hand distance from the antenna in semitones
-        let pitch_distance_semitones = IntervalF::new(pitch_coord_semitones.norm());
-        // Convert to note, based on the current note range
-        let raw_note = (*note_range.end() - pitch_distance_semitones)
-            .clamp(*note_range.start(), *note_range.end());
-
-        // Floating number of played chords. 2.5 means 2 notes and one half volume.
-        let note_number_height =
-            controls::convert_range(position.y, &(350.0..=500.0), &(1.0..=4.0));
-        let lead_volumes =
-            [0.0, 1.0, 2.0, 3.0].map(|v| (note_number_height.clamp(1.0, 4.0) - v).clamp(0.0, 1.0));
-
-        guitar_gates = lead_volumes.map(|v| v > 0.0);
-
-        // Autotune amount
-        let autotune =
-            controls::convert_range(hand.pinch_strength(), &(0.0..=1.0), &(0.0..=5.0)) as usize;
-
-        // Base note, autotuned
-        let note = restricted_scale_window.autotune(raw_note, autotune);
-
-        // Autochord, from the autotuned note so that the chord itself is autotuned
-        let chord = full_scale_window.autochord(note, &[0, 2, 4, 7]);
-
-        let lead_offset = preset.lead_interval_f();
-        let pluck_offset = preset.pluck_interval_f();
-
-        let pitch_bend = controls
-            .pitch_bend
-            .get_scaled(velocity.x + velocity.z, &(-300.0..=300.0));
-
-        // Send to dsp
-        for (control, value) in controls.lead.iter().zip(lead_volumes) {
-            control.volume.send(dsp_tx, value)?;
-        }
-
-        for (i, note) in chord.iter().enumerate() {
-            if let Some(note) = note {
-                controls.lead[i].send_note(dsp_tx, &(*note + lead_offset))?;
-                controls.strum[i].send_note(dsp_tx, &(*note + pluck_offset))?;
-            }
-        }
-        controls.strum_drone.send_note(
+impl<'a> LeapReader<'a> {
+    fn new(
+        controls: &'a controls::Controls,
+        ui_tx: &'a Sender<UiUpdate>,
+        dsp_tx: &'a Sender<dsp_thread::ParameterUpdate>,
+    ) -> Self {
+        Self {
+            controls,
+            ui_tx,
             dsp_tx,
-            &(preset.root_note_f() + pluck_offset + IntervalF::new(12.0)),
-        )?;
-
-        controls.pitch_bend.send(dsp_tx, pitch_bend)?;
-
-        // Send to UI
-        ui_tx.send(UiUpdate::AutotuneAmount(autotune))?;
-        ui_tx.send(UiUpdate::LeadChordNotes(
-            chord.map(|c| c.unwrap_or_default()),
-        ))?;
-        ui_tx.send(UiUpdate::LeadChordVolumes(lead_volumes))?;
-        ui_tx.send(UiUpdate::RawNote(raw_note))?;
-        ui_tx.send(UiUpdate::PitchXY(
-            pitch_coord_semitones.x * hand.x_factor(),
-            pitch_coord_semitones.y,
-        ))?;
-        ui_tx.send(UiUpdate::ChordsNumber(note_number_height))?;
-    }
-    if let Some(hand) = volume_hand {
-        let position = hand.position_from_body();
-        let velocity = hand.velocity_from_body();
-        let palm_normal = Vector3::from(hand.palm().normal().array());
-        let palm_dot = palm_normal.dot(&Vector3::y());
-        let strum_ready = hand.pinch_strength() > 0.9;
-        let trumpet = controls
-            .drone_trumpet
-            .get_scaled(velocity.y.abs(), &(0.0..=350.0));
-        if strum_ready {
-            for (i, string) in &mut controls.strum.iter().enumerate() {
-                string
-                    .pluck
-                    .send(dsp_tx, palm_dot > 0.0 + (i as f32) * 0.2 && guitar_gates[i]);
-            }
-            controls
-                .strum_drone
-                .pluck
-                .send(dsp_tx, preset.drone.pluck_drone && palm_dot > 0.1);
         }
-        let pluck_mute = controls.pluck_mute.get_scaled(palm_dot, &(-1.0..=0.0));
-        let cutoff_note_norm =
-            controls::convert_range(position.x, &(50.0..=200.0), &(-1.0..=1.0)).clamp(-1.0, 1.0);
-        let cutoff_note = controls
-            .cutoff_note
-            .get_scaled(cutoff_note_norm, &(-1.0..=1.0));
-        let resonance_norm =
-            controls::convert_range(position.z, &(100.0..=-100.0), &(0.0..=1.0)).clamp(0.0, 1.0);
-        let resonance = controls.resonance.get_scaled(resonance_norm, &(0.0..=1.0));
-        let lead_volume = controls
-            .lead_volume
-            .get_scaled(position.y, &(300.0..=400.0));
+    }
+}
 
-        // Send to dsp
-        controls.pluck_mute.send(dsp_tx, pluck_mute)?;
-        controls.cutoff_note.send(dsp_tx, cutoff_note)?;
-        controls.lead_volume.send(dsp_tx, lead_volume)?;
-        controls.resonance.send(dsp_tx, resonance)?;
-        controls.drone_trumpet.send(dsp_tx, trumpet)?;
-
-        // Send to UI
-        ui_tx.send(UiUpdate::Filter(
-            cutoff_note_norm * hand.x_factor(),
-            resonance_norm,
-        ))?;
-        ui_tx.send(UiUpdate::LeadVolume(lead_volume))?;
-        ui_tx.send(UiUpdate::StrumReady(strum_ready))?;
-        ui_tx.send(UiUpdate::TrumpetStrength(trumpet))?;
+impl<'a> LeapReader<'a> {
+    fn read_and_update(&mut self, settings: &Settings, connection: &mut Connection) -> Result<()> {
+        match connection.poll(100) {
+            Ok(message) => {
+                if let Event::Tracking(e) = message.event() {
+                    self.on_tracking_event(e, settings)?;
+                }
+                self.ui_tx.send(UiUpdate::ErrorReset)?;
+            }
+            Err(err) => {
+                self.ui_tx.send(UiUpdate::Error(err.to_string()))?;
+            }
+        }
+        Ok(())
     }
 
-    ui_tx.send(UiUpdate::HasHands(
-        hands.iter().any(|h| h.hand_type() == HandType::Left),
-        hands.iter().any(|h| h.hand_type() == HandType::Right),
-    ))?;
+    fn on_tracking_event(&mut self, e: TrackingEvent<'_>, settings: &Settings) -> Result<()> {
+        let preset = &settings.current_preset;
+        let handedness = &settings.system.handedness;
 
-    Ok(())
+        // Retrieve the current scale 2 by 2 windows
+        let full_scale_window = preset.full_scale_floating_window();
+        let restricted_scale_window = preset.restricted_scale_floating_window();
+
+        // List of visible hands
+        let hands = e.hands();
+        let pitch_hand = hands
+            .iter()
+            .find(|h| h.hand_type() == pitch_hand_type(handedness));
+        let volume_hand = hands
+            .iter()
+            .find(|h| h.hand_type() == volume_hand_type(handedness));
+
+        // Guitar gates
+        let mut guitar_gates = [false, false, false, false];
+        if let Some(hand) = pitch_hand {
+            let position = hand.position_from_body();
+            let velocity = hand.velocity_from_body();
+
+            let note_range = preset.note_range_f();
+
+            // Position the virtual antenna and the pitch hand relative to it
+            let antenna_coord = Vector2::new(400.0, -200.0);
+            let pitch_coord_mm = antenna_coord - Vector2::new(position.x, position.z);
+            // Hand position in semitones
+            let pitch_coord_semitones = pitch_coord_mm / 15.0;
+            let pitch_coord_semitones =
+                Vector2::new(-pitch_coord_semitones.x, pitch_coord_semitones.y);
+            // Hand distance from the antenna in semitones
+            let pitch_distance_semitones = IntervalF::new(pitch_coord_semitones.norm());
+            // Convert to note, based on the current note range
+            let raw_note = (*note_range.end() - pitch_distance_semitones)
+                .clamp(*note_range.start(), *note_range.end());
+
+            // Floating number of played chords. 2.5 means 2 notes and one half volume.
+            let note_number_height =
+                controls::convert_range(position.y, &(350.0..=500.0), &(1.0..=4.0));
+            let lead_volumes = [0.0, 1.0, 2.0, 3.0]
+                .map(|v| (note_number_height.clamp(1.0, 4.0) - v).clamp(0.0, 1.0));
+
+            guitar_gates = lead_volumes.map(|v| v > 0.0);
+
+            // Autotune amount
+            let autotune =
+                controls::convert_range(hand.pinch_strength(), &(0.0..=1.0), &(0.0..=5.0)) as usize;
+
+            // Base note, autotuned
+            let note = restricted_scale_window.autotune(raw_note, autotune);
+
+            // Autochord, from the autotuned note so that the chord itself is autotuned
+            let chord = full_scale_window.autochord(note, &[0, 2, 4, 7]);
+
+            let lead_offset = preset.lead_interval_f();
+            let pluck_offset = preset.pluck_interval_f();
+
+            let pitch_bend = self
+                .controls
+                .pitch_bend
+                .get_scaled(velocity.x + velocity.z, &(-300.0..=300.0));
+
+            // Send to dsp
+            for (control, value) in self.controls.lead.iter().zip(lead_volumes) {
+                control.volume.send(self.dsp_tx, value)?;
+            }
+
+            for (i, note) in chord.iter().enumerate() {
+                if let Some(note) = note {
+                    self.controls.lead[i].send_note(self.dsp_tx, &(*note + lead_offset))?;
+                    self.controls.strum[i].send_note(self.dsp_tx, &(*note + pluck_offset))?;
+                }
+            }
+            self.controls.strum_drone.send_note(
+                self.dsp_tx,
+                &(preset.root_note_f() + pluck_offset + IntervalF::new(12.0)),
+            )?;
+
+            self.controls.pitch_bend.send(self.dsp_tx, pitch_bend)?;
+
+            // Send to UI
+            self.ui_tx.send(UiUpdate::AutotuneAmount(autotune))?;
+            self.ui_tx.send(UiUpdate::LeadChordNotes(
+                chord.map(|c| c.unwrap_or_default()),
+            ))?;
+            self.ui_tx.send(UiUpdate::LeadChordVolumes(lead_volumes))?;
+            self.ui_tx.send(UiUpdate::RawNote(raw_note))?;
+            self.ui_tx.send(UiUpdate::PitchXY(
+                pitch_coord_semitones.x * hand.x_factor(),
+                pitch_coord_semitones.y,
+            ))?;
+            self.ui_tx
+                .send(UiUpdate::ChordsNumber(note_number_height))?;
+        }
+        if let Some(hand) = volume_hand {
+            let position = hand.position_from_body();
+            let velocity = hand.velocity_from_body();
+            let palm_normal = Vector3::from(hand.palm().normal().array());
+            let palm_dot = palm_normal.dot(&Vector3::y());
+            let strum_ready = hand.pinch_strength() > 0.9;
+            let trumpet = self
+                .controls
+                .drone_trumpet
+                .get_scaled(velocity.y.abs(), &(0.0..=350.0));
+            if strum_ready {
+                for (i, string) in &mut self.controls.strum.iter().enumerate() {
+                    string.pluck.send(
+                        self.dsp_tx,
+                        palm_dot > 0.0 + (i as f32) * 0.2 && guitar_gates[i],
+                    );
+                }
+                self.controls
+                    .strum_drone
+                    .pluck
+                    .send(self.dsp_tx, preset.drone.pluck_drone && palm_dot > 0.1);
+            }
+            let pluck_mute = self.controls.pluck_mute.get_scaled(palm_dot, &(-1.0..=0.0));
+            let cutoff_note_norm =
+                controls::convert_range(position.x, &(50.0..=200.0), &(-1.0..=1.0))
+                    .clamp(-1.0, 1.0);
+            let cutoff_note = self
+                .controls
+                .cutoff_note
+                .get_scaled(cutoff_note_norm, &(-1.0..=1.0));
+            let resonance_norm =
+                controls::convert_range(position.z, &(100.0..=-100.0), &(0.0..=1.0))
+                    .clamp(0.0, 1.0);
+            let resonance = self
+                .controls
+                .resonance
+                .get_scaled(resonance_norm, &(0.0..=1.0));
+            let lead_volume = self
+                .controls
+                .lead_volume
+                .get_scaled(position.y, &(300.0..=400.0));
+
+            // Send to dsp
+            self.controls.pluck_mute.send(self.dsp_tx, pluck_mute)?;
+            self.controls.cutoff_note.send(self.dsp_tx, cutoff_note)?;
+            self.controls.lead_volume.send(self.dsp_tx, lead_volume)?;
+            self.controls.resonance.send(self.dsp_tx, resonance)?;
+            self.controls.drone_trumpet.send(self.dsp_tx, trumpet)?;
+
+            // Send to UI
+            self.ui_tx.send(UiUpdate::Filter(
+                cutoff_note_norm * hand.x_factor(),
+                resonance_norm,
+            ))?;
+            self.ui_tx.send(UiUpdate::LeadVolume(lead_volume))?;
+            self.ui_tx.send(UiUpdate::StrumReady(strum_ready))?;
+            self.ui_tx.send(UiUpdate::TrumpetStrength(trumpet))?;
+        }
+
+        if let (Some(p), Some(v)) = (pitch_hand, volume_hand) {
+            if p.grab_strength() > 0.9 && v.grab_strength() > 0.9 {}
+        }
+
+        self.ui_tx.send(UiUpdate::HasHands(
+            hands.iter().any(|h| h.hand_type() == HandType::Left),
+            hands.iter().any(|h| h.hand_type() == HandType::Right),
+        ))?;
+
+        Ok(())
+    }
 }
 
 fn pitch_hand_type(handedness: &Handedness) -> leaprs::HandType {
